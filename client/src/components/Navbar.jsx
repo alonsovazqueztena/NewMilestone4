2fef44d8b889d90e73b09cb5f0c2ca81ec7d7d3c
Big Boss <saraviad4444@gmail.com>
2024-04-07 21:53:02 -0700

    navbar comments

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom"
+// Import the Link component from react-router-dom to handle routing
+import { Link } from "react-router-dom";
+
+// Define the Navbar functional component
 export default function Navbar() {
+  // Return statement contains the JSX for the Navbar
   return (
+    // Use the <nav> element to define the navigation links area
     <nav>
-      <Link to='/' >Home</Link>
-      <Link to='/register' >Register</Link>
-      <Link to='/login' >Login</Link>
+      {/* Link components are used for navigation. 
+          `to` prop specifies the route to navigate to when the link is clicked. */}
+      <Link to='/'>Home</Link> {/* Link to the Home page */}
+      <Link to='/register'>Register</Link> {/* Link to the Register page */}
+      <Link to='/login'>Login</Link> {/* Link to the Login page */}
     </nav>
-  )
+  );
 }
